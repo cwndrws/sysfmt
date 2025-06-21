114ff54b4e9d7f8010303cc6c7775d55ebdb9c12
Charlie Andrews <charlieandrews.cwa@gmail.com>
2025-06-21 06:31:21 -0400

    Setup flake to get nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A very small utility for filling out template strings with system stats";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    usershell.url = "github:cwndrws/usershell";
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, usershell, zig-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          zig-nightly = zig-overlay.packages.${system}.master;
+        in
+        {
+          devShells.default = usershell.lib.mkUserShell {
+            inherit pkgs;
+            buildInputs = [ zig-nightly ];
+          };
+        }
+      );
+}
+
